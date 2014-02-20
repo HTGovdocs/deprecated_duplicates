e8d4c8784c136e652121f99e0ab9a20074beeed1
Martin Warin <mwarin@umich.edu>
2014-02-20 10:26:56 -0500

    mostly whitespace, rewrote a query to use explicit JOIN

--- scripts/heuristics.rb
+++ scripts/heuristics.rb
@@ -1,11 +1,11 @@
-$LOAD_PATH << './lib/'
+$LOAD_PATH << './../lib/'
 require 'gddb';
 
 db    = Gddb::Db.new();
 @conn = db.get_interactive();
 
-get_vals_sql   = "SELECT gp.prop, gp.val, COUNT(gp.val) AS cv FROM mwarin_ht.gd_prop AS gp, mwarin_ht.gd_str AS gs WHERE gp.prop = gs.gd_str_id AND gs.str = ? GROUP BY gp.prop, gp.val HAVING cv >= 2";
-@get_vals_q    = @conn.prepare(get_vals_sql);
+get_vals_sql = "SELECT gp.prop, gp.val, COUNT(gp.val) AS cv FROM mwarin_ht.gd_prop AS gp JOIN mwarin_ht.gd_str AS gs ON (gp.prop = gs.gd_str_id) WHERE gs.str = ? GROUP BY gp.prop, gp.val HAVING cv >= 2"
+@get_vals_q  = @conn.prepare(get_vals_sql);
 
 get_by_val_sql = "SELECT gd_item_id FROM mwarin_ht.gd_prop WHERE prop = ? AND val = ?"
 @get_by_val_q  = @conn.prepare(get_by_val_sql);
@@ -16,10 +16,11 @@ insert_cluster_sql = "INSERT INTO mwarin_ht.gd_cluster () VALUES ()";
 insert_cluster_item_sql = "INSERT INTO mwarin_ht.gd_item_cluster (gd_item_id, gd_cluster_id) VALUES (?, ?)";
 @insert_cluster_item_q  = @conn.prepare(insert_cluster_item_sql);
 
-last_id_sql    = "SELECT LAST_INSERT_ID() AS id";
-@last_id_q     = @conn.prepare(last_id_sql);
+last_id_sql = "SELECT LAST_INSERT_ID() AS id";
+@last_id_q  = @conn.prepare(last_id_sql);
 
-["DELETE FROM mwarin_ht.gd_item_cluster", "DELETE FROM mwarin_ht.gd_cluster"].each do |dq|
+["DELETE FROM mwarin_ht.gd_item_cluster", 
+ "DELETE FROM mwarin_ht.gd_cluster"].each do |dq|
   puts dq;
   @conn.update(dq);
 end
