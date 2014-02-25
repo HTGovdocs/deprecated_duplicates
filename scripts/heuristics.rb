91d80bc630118a291fdf36782ec145297cf450a1
Martin Warin <mwarin@umich.edu>
2014-02-25 14:43:05 -0500

    the main method contains everything

--- scripts/heuristics.rb
+++ scripts/heuristics.rb
@@ -9,10 +9,10 @@ db    = Gddb::Db.new();
 min_cluster_size = 2;
 max_cluster_size = 25;
 get_vals_sql = %W<
-    SELECT gp.prop, gp.val, COUNT(gp.val) AS cv 
-      FROM #{@schema}.gd_prop AS gp JOIN #{@schema}.gd_str AS gs ON (gp.prop = gs.gd_str_id) 
-     WHERE gs.str = ? 
-  GROUP BY gp.prop, gp.val 
+    SELECT gp.prop, gp.val, COUNT(gp.val) AS cv
+      FROM #{@schema}.gd_prop AS gp JOIN #{@schema}.gd_str AS gs ON (gp.prop = gs.gd_str_id)
+     WHERE gs.str = ?
+  GROUP BY gp.prop, gp.val
     HAVING cv BETWEEN #{min_cluster_size} AND #{max_cluster_size}
 >.join(' ');
 @get_vals_q  = @conn.prepare(get_vals_sql);
@@ -30,53 +30,31 @@ last_id_sql = "SELECT LAST_INSERT_ID() AS id";
 @last_id_q  = @conn.prepare(last_id_sql);
 
 [
- "DELETE FROM #{@schema}.gd_cluster_weights", 
- "DELETE FROM #{@schema}.gd_item_cluster", 
+ "DELETE FROM #{@schema}.gd_cluster_weights",
+ "DELETE FROM #{@schema}.gd_item_cluster",
  "DELETE FROM #{@schema}.gd_cluster"
 ].each do |dq|
   puts dq;
   @conn.update(dq);
 end
 
+# Not prepared here, but by prepare_qmarks as needed.
 @get_subvals_sql = "SELECT val, COUNT(val) AS cv FROM #{@schema}.gd_prop WHERE gd_item_id IN (__?__) AND prop = ? GROUP BY val HAVING cv > 1";
-
 @get_intersect_items_sql = "SELECT gd_item_id FROM #{@schema}.gd_prop WHERE gd_item_id IN (__?__) AND val = ?";
 
 insert_weight_sql = "INSERT INTO #{@schema}.gd_cluster_weights (gd_cluster_id, prop, weight) VALUES (?, ?, ?)"
 @insert_weight_q   = @conn.prepare(insert_weight_sql);
 
 # Keep prepared statements with lists of bind params of variable length
-@preps     = {}; 
+@preps     = {};
 
 # If we use this dict of props we don't need to join against gd_str to get human-readable props.
 @prop_dict = {};
 # Assuming it isn't going to be huge and that there will be no overlap between keys and values.
 @conn.query("SELECT prop, str FROM #{@schema}.v_gd_prop_str").each do |row|
   @prop_dict[row[:prop].to_i] = row[:str];
-  @prop_dict[row[:str]] = row[:prop].to_i;
+  @prop_dict[row[:str]]       = row[:prop].to_i;
 end
-puts @prop_dict;
-
-=begin # Display a cluster:
-SELECT
-    gic.gd_cluster_id,
-    gp.gd_item_id,
-    gp.prop,
-    vp.str AS propstr,
-    gp.val,
-    vv.str AS valstr
-FROM
-    gd_item_cluster    AS gic
-    JOIN gd_prop       AS gp ON (gic.gd_item_id = gp.gd_item_id)
-    JOIN v_gd_prop_str AS vp ON (gp.prop = vp.prop)
-    JOIN v_gd_val_str  AS vv ON (gp.val = vv.val)
-WHERE
-    gic.gd_cluster_id = 0 -- insert id here
-ORDER BY
-    gic.gd_cluster_id,
-    gp.gd_item_id, 
-    gp.prop;
-=end
 
 # Takes a list of ids (at least 2) and creates a cluster for them
 # and assigns them to that cluster.
@@ -92,7 +70,6 @@ def make_cluster (item_id_list, prop_names)
   end
 
   make_cluster_weights(gd_cluster_id, prop_names);
-
 end
 
 def make_cluster_weights (gd_cluster_id, prop_names)
@@ -123,9 +100,9 @@ def find_clusters (prop_names = [])
   end
 end
 
-# Gets items with the same something.
-# Have these return lists rather than making clusters on the spot,
-# that way we can perform joins / filters on said lists.
+# Gets items with the same property.
+# Returns an array of arrays, where each sub-array has the same
+# value for the property.
 def same (prop_name)
   clusters = [];
   @get_vals_q.enumerate(prop_name) do |row1|
@@ -181,10 +158,8 @@ def prepare_qmarks (sql, bindparams)
   return p;
 end
 
-if $0 == __FILE__ then
-
-
-  find_clusters(['title_series', 'agency']);
+def public_static_void_main
+  find_clusters(%w{title_series agency});
   find_clusters(%w{author title published});
   find_clusters('lccn');
   find_clusters('issn');
@@ -192,4 +167,8 @@ if $0 == __FILE__ then
   find_clusters('oclcnum');
 
   @conn.close();
+end
+
+if $0 == __FILE__ then
+  public_static_void_main()
 end
