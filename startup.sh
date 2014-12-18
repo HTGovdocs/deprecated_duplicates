c1ac12d03a3f23096458c2e7b9a852fcb68abe81
Martin Warin <mwarin@umich.edu>
2014-12-18 13:18:01 -0500

    indentation and reordering

--- startup.sh
+++ startup.sh
@@ -9,8 +9,10 @@ if [ -f $SCRIPTPATH/.env ]; then
     echo "there is already an .env file";
 else
     echo "Writing dummy .env file to $SCRIPTPATH/.env";
+    echo "data_dir_path  = $SCRIPTPATH/data" >> $SCRIPTPATH/.env;
+    echo "log_dir_path   = $SCRIPTPATH/log"  >> $SCRIPTPATH/.env;
 
-    cat << EOF > $SCRIPTPATH/.env
+    cat << EOF >> $SCRIPTPATH/.env
 ## Fill in these variables.
 db_driver = xx
 db_url    = xx
@@ -20,8 +22,5 @@ db_host   = xx
 db_name   = xx
 db_port   = xx
 EOF
-
-echo "data_dir_path  = $SCRIPTPATH/data" >> $SCRIPTPATH/.env;
-echo "log_dir_path   = $SCRIPTPATH/log"  >> $SCRIPTPATH/.env;
 
 fi
