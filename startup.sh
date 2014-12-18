971e3224859ec32e40494742fc09ad3dfe2384da
Martin Warin <mwarin@umich.edu>
2014-12-18 12:59:50 -0500

    Added startup script which creates data/, log/ and .env

---
+++ startup.sh
@@ -0,0 +1,24 @@
+pushd `dirname $0` > /dev/null;
+SCRIPTPATH=`pwd`;
+popd > /dev/null;
+
+mkdir -p $SCRIPTPATH/data;
+mkdir -p $SCRIPTPATH/log;
+
+if [ -f $SCRIPTPATH/.env ]; then
+    echo "there is already an .env file";
+else
+    echo "Writing dummy .env file to $SCRIPTPATH/.env";
+
+    cat << EOF > $SCRIPTPATH/.env
+## Fill in these variables.
+db_driver = xx
+db_url    = xx
+db_user   = xx
+db_pw     = xx
+db_host   = xx
+db_name   = xx
+db_port   = xx
+EOF
+
+fi
