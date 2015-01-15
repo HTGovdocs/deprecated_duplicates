2981e5b690bfebf996a316c9c6e5cdf0d80e0bbc
Martin Warin <mwarin@umich.edu>
2015-01-15 13:36:14 -0500

    htid -> record_id, added some doc

--- scripts/load_hathi_sudocs.rb
+++ scripts/load_hathi_sudocs.rb
@@ -1,12 +1,22 @@
 require 'htph';
 
+=begin
+
+Rather specific script, for adding sudoc to the hathi records. Only
+useful as long as we're using the hathifile for input. Run before
+adding any other members' data.
+
+bundle exec ruby scripts/load_hathi_sudocs.rb
+
+=end
+
 log = HTPH::Hathilog::Log.new();
 log.d("Started");
 
 db   = HTPH::Hathidb::Db.new();
 conn = db.get_conn();
 
-get_id_sql = "SELECT id FROM hathi_gd WHERE htid = ?";
+get_id_sql = "SELECT id FROM hathi_gd WHERE record_id = ?";
 get_id_q   = conn.prepare(get_id_sql);
 
 insert_str_sql = "INSERT INTO hathi_str (str) VALUES (?)";
@@ -30,7 +40,7 @@ HTPH::Hathidata.read("htid_sudoc.tsv") do |line|
   end
   (htid, sudoc) = line.strip.split("\t");  
   get_id_q.query(htid) do |get_id_row|
-    gd_id  = get_id_row[:id];
+    gd_id = get_id_row[:id];
     insert_str_q.execute(sudoc);
     last_id_q.query() do |get_last_row|
       str_id = get_last_row[:id];
