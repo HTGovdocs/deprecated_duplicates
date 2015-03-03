29fae9d20252fea539d34830b18498bf27d2427d
Martin Warin <mwarin@umich.edu>
2015-03-03 16:22:37 -0500

    added marc_field columns to the value-tables, and some indices

--- sql/hathi_gd.sql
+++ sql/hathi_gd.sql
@@ -12,8 +12,9 @@ drop table if exists hathi_sudoc;
 drop table if exists hathi_title;
 
 drop table if exists hathi_str;
-drop table if exists hathi_gd;
 drop table if exists hathi_input_file;
+drop table if exists hathi_gd;
+drop table if exists hathi_related;
 
 -- Keep track of files used as input.
 -- PK is used as FK in hathi_gd, to ensure that
@@ -31,7 +32,8 @@ create table hathi_gd (
   id        INT         not null auto_increment,
   gov_doc   TINYINT     not null,
   file_id   INT         not null,
-  record_id VARCHAR(50) not null, -- Whatever the unique id is in the file
+  record_id VARCHAR(50) not null, -- Whatever the unique id is of the base record in the file.
+  item_id  VARCHAR(50)  null,     -- If the record has holdings, the id of the item.
   hashsum   CHAR(64)    not null unique,
   primary key (id),
   foreign key (file_id) references hathi_input_file(id)
@@ -49,6 +51,7 @@ create table hathi_str (
 create table hathi_oclc (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -57,6 +60,7 @@ create table hathi_oclc (
 create table hathi_lccn (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -65,6 +69,7 @@ create table hathi_lccn (
 create table hathi_issn (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -73,6 +78,7 @@ create table hathi_issn (
 create table hathi_isbn (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -81,6 +87,7 @@ create table hathi_isbn (
 create table hathi_title (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -89,6 +96,7 @@ create table hathi_title (
 create table hathi_enumc (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -97,6 +105,7 @@ create table hathi_enumc (
 create table hathi_pubdate (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -105,6 +114,7 @@ create table hathi_pubdate (
 create table hathi_publisher (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
@@ -113,10 +123,47 @@ create table hathi_publisher (
 create table hathi_sudoc (
   gd_id  INT not null,
   str_id INT not null,
+  marc_field CHAR(4) not null,
   primary key (gd_id, str_id),
   foreign key (gd_id) references hathi_gd(id),
   foreign key (str_id) references hathi_str(id)
 );
 
+-- Contains ids and hashes. Ids with the same hash are (at least) related.
+create table hathi_related (
+  gd_id    INT      not null,
+  checksum CHAR(32) not null,
+  foreign key (gd_id) references hathi_gd(id)
+);
+
 create index hathi_gd_str on hathi_str (str) using btree;
 create index hathi_record on hathi_gd (record_id) using btree;
+create index related_checksum on hathi_related (checksum) using hash;
+create index related_gd_id on hathi_related (gd_id) using btree;
+
+create index hathi_sudoc_gd_id  on hathi_sudoc (gd_id)  using btree;
+create index hathi_sudoc_str_id on hathi_sudoc (str_id) using btree;
+
+create index hathi_publisher_gd_id  on hathi_publisher (gd_id)  using btree;
+create index hathi_publisher_str_id on hathi_publisher (str_id) using btree;
+
+create index hathi_pubdate_gd_id  on hathi_pubdate (gd_id)  using btree;
+create index hathi_pubdate_str_id on hathi_pubdate (str_id) using btree;
+
+create index hathi_enumc_gd_id  on hathi_enumc (gd_id)  using btree;
+create index hathi_enumc_str_id on hathi_enumc (str_id) using btree;
+
+create index hathi_title_gd_id  on hathi_title (gd_id)  using btree;
+create index hathi_title_str_id on hathi_title (str_id) using btree;
+
+create index hathi_isbn_gd_id  on hathi_isbn (gd_id)  using btree;
+create index hathi_isbn_str_id on hathi_isbn (str_id) using btree;
+
+create index hathi_issn_gd_id  on hathi_issn (gd_id)  using btree;
+create index hathi_issn_str_id on hathi_issn (str_id) using btree;
+
+create index hathi_lccn_gd_id  on hathi_lccn (gd_id)  using btree;
+create index hathi_lccn_str_id on hathi_lccn (str_id) using btree;
+
+create index hathi_oclc_gd_id  on hathi_oclc (gd_id)  using btree;
+create index hathi_oclc_str_id on hathi_oclc (str_id) using btree;
