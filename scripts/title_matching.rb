f01ef584905f25c8da4add4ce52d07289143a844
Martin Warin <mwarin@umich.edu>
2016-01-22 12:24:50 -0500

    added cmdline flags for verbosity and thread count

--- scripts/title_matching.rb
+++ scripts/title_matching.rb
@@ -1,15 +1,40 @@
 require 'htph';
 require 'threach';
 
-# Use for documents that lack good identifiers.
+# Use this script for documents that lack good identifiers.
+# Once you have a list of documents that couldn't be clustered
+# based on their identifiers, run this.
 
-# Take a file of ids
+# Turn on verbosity with -v. Defaults to false.
+$verbose = false;
+v_flag = ARGV.select{|arg| arg =~ /^-v$/}
+if !v_flag.empty? then
+  ARGV.delete(v_flag.first);
+  $verbose = true;
+end
+
+# Set the thread count with -t=<int>, defaults to 4.
+$thread_count = 4;
+t_flag = ARGV.select{|arg| arg =~ /^-t=\d+$/}
+if !t_flag.empty? then
+  ARGV.delete(t_flag[0]);
+  t_flag.first =~ /(\d+)/;
+  $thread_count = Integer($1);
+end
+
+# Only bother analyzing document pairs that have at least overlap_min_size words in common.
+overlap_min_size = 5;
+
+# Only output pairs with a score greater than score_cutoff.
+score_cutoff     = 0.5;
+
+# Take a file of ids as input.
 file = ARGV.shift;
 hdin = HTPH::Hathidata::Data.new(file).open('r');
+
 db   = HTPH::Hathidb::Db.new();
 conn = db.get_conn();
 log  = HTPH::Hathilog::Log.new();
-$verbose = false;
 
 # Look up ids and make bags of words (one for title, one for publisher, etc for all categories we want)
 categories  = %w[publisher title pubdate enumc];
@@ -22,10 +47,7 @@ stop_words  = %w[
   THE TO UNITED U US
 ];
 
-score_cutoff     = 0.5;
-overlap_min_size = 5;
-thread_count     = 4;
-
+# Get the string value of XXX (each of categories) given a gd_id.
 cat_sql_template = %w<
   SELECT hs.str
   FROM hathi_XXX AS hx
@@ -41,12 +63,16 @@ end
 # Populate id bags
 id_bags = {};
 i = 0;
-hdin.file.each_line do |id|
+# A line can be just a gd_id or "solo\t<gd_id>"
+hdin.file.each_line do |line|
   i += 1;
   log.d("Read #{i} lines") if i % 1000 == 0;
-  id.strip!;
-  id = Integer(id);
+  line.strip!;
+  line.sub!("solo\t", '');
+  id = Integer(line);
+  # Make bag.
   id_bags[id] = {};
+  # For each categort, get all the words an put in bag.
   categories.each do |c|
     id_bags[id][c] = [];
     cat_queries[c].enumerate(id) do |row|
@@ -63,7 +89,6 @@ hdin.file.each_line do |id|
     end
   end
 end
-
 hdin.close();
 conn.close();
 
@@ -71,19 +96,20 @@ tot_freq = 0.0;
 word_freqs.values.each do |freq|
   tot_freq += freq;
 end
+
 # Inverse word/doc freq (because a word only occurs 0 or 1 times per doc, so word freq and doc freq are the same)
 # would be 1 - (word_freq.to_f / tot_freq)
 inv_freq    = lambda {|word| 1 - (word_freqs[word].to_f / tot_freq).round(3)};
 get_words   = lambda {|id|   categories.map{|c| id_bags[id][c]}.flatten.sort.uniq};
 comparisons = 0;
 outputted   = 0;
-hdout       = HTPH::Hathidata::Data.new("title_word_matches.tsv").open('w');
+hdout       = HTPH::Hathidata::Data.new("title_word_matches_$ymd.tsv").open('w');
 
 # Writing to outfile needs to be synchronized.
 mutex = Mutex.new();
 
 # N.B. threaded loop.
-id_bags.keys.sort.threach(thread_count) do |id|
+id_bags.keys.sort.threach($thread_count) do |id|
   # Get all the words per id
   # Sort so the first word is the most relevant/rare
   words = get_words.call(id).sort_by{|w| inv_freq.call(w)}.reverse;
@@ -94,7 +120,7 @@ id_bags.keys.sort.threach(thread_count) do |id|
   other_ids.select{|x| x > id}.each do |other_id|
     other_words = get_words.call(other_id);
     # & works as set intersection
-    overlap     = words & other_words;
+    overlap = words & other_words;
     # Don't bother unless they have enough words in common.
     if (overlap.size == words.size || overlap.size > overlap_min_size) then
       # The words that only occur in one of the sets
@@ -104,12 +130,16 @@ id_bags.keys.sort.threach(thread_count) do |id|
       score            = (overlap_tot_freq - misses_tot_freq) / (overlap.size + misses.size);
 
       if $verbose == true then
-        puts "words #{words.join(',')}";
-        puts "other #{other_words.join(',')}";
-        puts "overlap #{overlap.join(',')}";
-        puts "misses #{misses.join(',')}";
-        puts "score (#{overlap_tot_freq} - #{misses_tot_freq}) / (#{overlap.size} + #{misses.size}) = #{score}";
-        puts "---";
+        # Verbosity will slow things down a bit since there'd be more to sync.
+        mutex.synchronize {
+          puts "#{id} vs #{other_id}:";
+          puts "words #{words.join(',')}";
+          puts "other #{other_words.join(',')}";
+          puts "overlap #{overlap.join(',')}";
+          puts "misses #{misses.join(',')}";
+          puts "score (#{overlap_tot_freq} - #{misses_tot_freq}) / (#{overlap.size} + #{misses.size}) = #{score}";
+          puts "---";
+        }
       end
 
       # Arbitrary cutoff. Only output if the score is high enough.
@@ -129,7 +159,6 @@ id_bags.keys.sort.threach(thread_count) do |id|
       if comparisons % 10000 == 0 then
         log.d("#{comparisons} record pairs compared, #{outputted} outputted");
       end
-
     end
   end
 end
