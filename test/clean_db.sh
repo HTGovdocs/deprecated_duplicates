1c287eef298701c2b34d96b154458d88b8dc576b
Martin Warin <mwarin@umich.edu>
2016-02-01 09:56:49 -0500

    get db stuff from .env

--- test/clean_db.sh
+++ test/clean_db.sh
@@ -5,6 +5,8 @@ pushd `dirname $0` > /dev/null;
 SCRIPTPATH=`pwd`;
 popd > /dev/null;
 
+dbhost=`grep -Po '^db_host\s+=\s+.+' $SCRIPTPATH/../.env | awk '{print $3}'`;
+
 # Drop everything and rebuild empty tables.
 echo "Refreshing database, gonna need mysql password for user `whoami`";
 mysql --verbose --show-warnings -h mysql-htprep -p -D gd_test < $SCRIPTPATH/../sql/hathi_gd.sql;
